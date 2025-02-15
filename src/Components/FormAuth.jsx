412302e0d20c2e17c9099187e22f6f50aa5ba8c6
Florent <florent.guyard@live.fr>
2025-02-15 16:50:41 +0100

    design auth page

--- src/Components/FormAuth.jsx
+++ src/Components/FormAuth.jsx
@@ -48,47 +48,55 @@ const AuthForm = () => {
       <div className="form_container" >
         <h2 className="form_title">{isLogin ? 'Connexion' : 'Inscription'}</h2>
         <form onSubmit={handleSubmit} className="form_form">
-          {/* Champ e-mail */}
-          <div className="form_field" >
-            <label htmlFor="username" className="form_label">
-              Email :
-            </label>
-            <input type="text" id="text" name="username" required className="form_input" />
-          </div>
-  
-          {/* Champ mot de passe */}
-          <div className="form_field" >
-            <label htmlFor="password" className="form_label">
-              Mot de passe :
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              required
-              className="form_input"
-            />
-          </div>
-  
-          {/* Champ confirmation de mot de passe (inscription seulement) */}
-          {!isLogin && (
-            <div className="form_field">
-              <label htmlFor="confirmPassword" className="form_label">
-                Confirmer le mot de passe :
+          <div>
+            {/* Champ e-mail */}
+            <div className="form_field" >
+              <label htmlFor="username" className="form_label">
+                Email :
+              </label>
+              <input type="text" id="text" name="username" required className="form_input" />
+            </div>
+    
+            {/* Champ mot de passe */}
+            <div className="form_field" >
+              <label htmlFor="password" className="form_label">
+                Mot de passe :
               </label>
               <input
                 type="password"
-                id="confirmPassword"
-                name="confirmPassword"
+                id="password"
+                name="password"
                 required
                 className="form_input"
               />
             </div>
-          )}
-  
-          <button type="submit" className="form_button">
-            {isLogin ? 'Se connecter' : "S'inscrire"}
-          </button>
+
+            {!isLogin && (
+              <div className="form_field">
+                <label htmlFor="confirmPassword" className="form_label">
+                  Confirmer le mot de passe :
+                </label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  required
+                  className="form_input"
+                />
+              </div>
+            )}
+
+            <div className="div_button">
+              <button type="submit" className="form_button ">
+                {isLogin ? 'Se connecter' : "S'inscrire"}
+              </button>
+            </div>
+            </div>
+    
+            
+    
+            
+          
         </form>
   
         {/* Bascule entre connexion et inscription */}
